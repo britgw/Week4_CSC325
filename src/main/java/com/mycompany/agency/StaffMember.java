614bac6b5ec63818af439b65237219f2d19d0cde
darbt <darbt3@farmingdale.edu>
2022-09-22 12:49:45 -0400

    test

--- src/main/java/com/mycompany/agency/StaffMember.java
+++ src/main/java/com/mycompany/agency/StaffMember.java
@@ -3,7 +3,7 @@ package com.mycompany.agency;
 
 
 // TODO 1: Make this class work and public
- class StaffMember
+ public abstract class StaffMember
 {
     protected String name;
     protected String address;
@@ -25,7 +25,13 @@ package com.mycompany.agency;
     //-----------------------------------------------------------------
     public String toString()
     {
-
+        String result= " ";
+        //for loop
+        result = "Employee - "++
+                "\nName: "+name+
+                 "\nAddress: "+address+
+                 "\nPhone: "+phone+"\n";
+                return result; 
     }
 
     //-----------------------------------------------------------------
